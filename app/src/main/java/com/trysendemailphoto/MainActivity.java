e560f6aed76877910690266d65842d3ffb230322
roy.leung <Roy.Leung@RoyLeungs-MacBook-Air.local>
2017-02-28 11:53:34 +0800

    layout create

--- app/src/main/java/com/trysendemailphoto/MainActivity.java
+++ app/src/main/java/com/trysendemailphoto/MainActivity.java
@@ -1,13 +1,31 @@
 package com.trysendemailphoto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.edEmail)
+    EditText edEmail;
+    @BindView(R.id.edMessage)
+    EditText edMessage;
+    @BindView(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onClick() {
     }
 }
